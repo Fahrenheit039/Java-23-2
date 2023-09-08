e2f6032359d9b71dfd531812e485a6fed7666c68
Fahrenheit039 <protoss004@mail.ru>
2023-09-08 13:33:22 +0200

    hierarchy

---
+++ lab1/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        problem1();
+        problem2();
+        problem3();
+    }
+
+    public static void problem1(){
+        //TODO
+        System.out.println("123123123");
+    }
+    public static void problem2(){
+        //TODO
+        System.out.println("123123123");
+    }
+    public static void problem3(){
+        //TODO
+        System.out.println("123123123");
+    }
+}
